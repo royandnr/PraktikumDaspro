acc04bdc6efc6463e357584a5d325ef8c6305fd2
Yann <yann@gmail.com>
2025-10-10 15:08:45 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS23.java
+++ jobsheet5/ifCetakKRS23.java
@@ -2,20 +2,18 @@ package jobsheet5;
 import java.util.Scanner;
 
 public class ifCetakKRS23 {
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    System.out.println("--- Cetak KRS SIAKAD ---");
-    System.out.print("Apakah UKT sudah lunas? (true/false) : ");
-    boolean uktLunas = sc.nextBoolean();
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false) : ");
+        boolean uktLunas = sc.nextBoolean();
 
-    if (uktLunas) {
-    System.out.println("Pembayaran UKT terferivikasi");
-    System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        String hasil = (uktLunas) 
+            ? "Pembayaran UKT terverifikasi\nSilakan cetak KRS dan minta tanda tangan DPA"
+            : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.";
+
+        System.out.println(hasil);
     }
-    else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.");
-        }
-
-} 
 }
+
