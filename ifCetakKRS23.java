f867b0caea48d6091d2460c65e23155518caa927
Yann <yann@gmail.com>
2025-10-10 11:16:57 +0700

    Percobaan 1

---
+++ ifCetakKRS23.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ifCetakKRS23 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("--- Cetak KRS SIAKAD ---");
+    System.out.print("Apakah UKT sudah lunas? (true/false) : ");
+    boolean uktLunas = sc.nextBoolean();
+
+    if (uktLunas) {
+    System.out.println("Pembayaran UKT terferivikasi");
+    System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+    }
+
+} 
+}
